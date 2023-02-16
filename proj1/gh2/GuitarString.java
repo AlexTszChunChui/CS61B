4bd44082d1f4171e65e92d7479cef86dd7084980
AlexTszChunChui <121783652+AlexTszChunChui@users.noreply.github.com>
2023-02-16 11:02:00 -0500

    Update GuitarString.java

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,8 +1,10 @@
 package gh2;
 
 // TODO: uncomment the following import once you're ready to start this portion
-// import deque.Deque;
+import deque.Deque;
 // TODO: maybe more imports
+
+import deque.LinkedListDeque;
 
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
@@ -14,7 +16,7 @@ public class GuitarString {
 
     /* Buffer for storing sound data. */
     // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque<Double> buffer;
+    private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
@@ -22,6 +24,12 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer array with zeros.
+        double capacity = SR / frequency;
+        int bettercapactiy = (int) Math.round(capacity);
+        buffer = new LinkedListDeque<Double>();
+        for (int x = 0; x < bettercapactiy; x += 1) {
+            buffer.addFirst((double) 0);
+        }
     }
 
 
