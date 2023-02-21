7ed9576b7b1521bd995347603900ffbdcc12f106
AlexTszChunChui <121783652+AlexTszChunChui@users.noreply.github.com>
2023-02-21 03:19:24 -0500

    lab6

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,6 +1,8 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
+
 import static capers.Utils.*;
 
 /** A repository for Capers 
@@ -18,8 +20,7 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = Utils.join(CWD, ".capers");
 
     /**
      * Does required filesystem operations to allow for persistence.
@@ -31,7 +32,19 @@ public class CapersRepository {
      *    - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        File c = new File(".capers");
+        c.mkdir();
+        try {
+            File s = new File(".capers", "story.txt");
+            s.createNewFile();
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+
+        File d = new File(".capers", "dogs");
+        d.mkdir();
+
     }
 
     /**
@@ -40,7 +53,10 @@ public class CapersRepository {
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        File story = Utils.join(CAPERS_FOLDER, "story.txt");
+        text = readContentsAsString(story) + text + "\n";
+        System.out.println(text);
+        Utils.writeContents(story, text);
     }
 
     /**
@@ -49,7 +65,7 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog dog = new Dog(name, breed, age);
     }
 
     /**
@@ -59,6 +75,7 @@ public class CapersRepository {
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog dog = Dog.fromFile(name);
+        dog.haveBirthday();
     }
 }
--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,17 +1,17 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
  * @author TODO
 */
-public class Dog { // TODO
+public class Dog implements Serializable { // TODO
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = Utils.join(".capers" , "dogs");
 
     /** Age of dog. */
     private int age;
@@ -30,6 +30,8 @@ public class Dog { // TODO
         this.age = age;
         this.breed = breed;
         this.name = name;
+        this.saveDog();
+        System.out.println(this.toString());
     }
 
     /**
@@ -40,7 +42,9 @@ public class Dog { // TODO
      */
     public static Dog fromFile(String name) {
         // TODO (hint: look at the Utils file)
-        return null;
+        File file = Utils.join(DOG_FOLDER, name);
+        Dog dog = readObject(file, Dog.class);
+        return dog;
     }
 
     /**
@@ -48,6 +52,7 @@ public class Dog { // TODO
      */
     public void haveBirthday() {
         age += 1;
+        this.saveDog();
         System.out.println(toString());
         System.out.println("Happy birthday! Woof! Woof!");
     }
@@ -56,7 +61,14 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File dog = new File(DOG_FOLDER, name);
+        try {
+            dog.createNewFile();
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+        writeObject(dog, this);
     }
 
     @Override
